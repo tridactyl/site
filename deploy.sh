aad026065c9ee3d1a8b26534c439b85658888422
Oliver Blanthorn <freedom4cows@gmail.com>
2020-02-05 11:35:21 +0000

    Fix annoyances with deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,16 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+cd public
+
+git reset --hard
+
+git checkout master
+
+git pull
+
+cd -
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
